3c82a0c22d207e9c54e546e0be0058d000328f1c
Allen <allen@ubuntu.ubuntu-domain>
2013-02-22 13:19:28 +0800

    change posts default order

--- app/models/post.rb
+++ app/models/post.rb
@@ -19,6 +19,7 @@ class Post < ActiveRecord::Base
   has_attached_file :attachment
   before_create :auto_approve_when_admin
   has_many :uploads,:dependent => :destroy
+  default_scope order('created_at DESC')
   
   GRADE_LIST = [["七年级",7],["八年级",8],["九年级",9],["后勤",13]]
   ORG_LIST = [["音乐美术部",1],["体育俱乐部",2],["科普小组",3]]
