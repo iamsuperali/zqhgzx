3084fb59198a346d72728c31c626d1336766cfa3
Allen <iamsuperali@126.com>
2013-05-02 10:43:17 +0800

    admin.js 和 admin.css 需要独立压缩

--- app/assets/javascripts/text_cut.js
+++ app/assets/javascripts/text_cut.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
     $("[class^='textcut']").each(function() {
-        var cutlength = parseInt($(this).attr('class').replace("textcut", ""));
-        var text = $(this).html().trim();
-        $(this).attr("title", text);
+        var cutlength = parseInt($(this).prop('class').replace("textcut", ""));
+        var text = $.trim( $(this).html() );
+        $(this).prop("title", text); 
         text = (text.length > cutlength + 2) ? (text.substring(0, cutlength) + "...") : text;
         $(this).html(text);
     });
