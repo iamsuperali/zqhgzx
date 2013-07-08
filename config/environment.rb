a27a546a3bddafb5012afdcc87a048e3a31ac446
Allen <iamsuperali@126.com>
2013-07-08 12:20:02 +0800

    smtp

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 FishCms::Application.initialize!
+
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+  :address        => "smtp.163.com",
+  :port           => 25,
+  :domain         => "www.163.com",
+  :authentication => :login,
+  :user_name      => "fishcms@163.com",
+  :password       => "13602953927"
+}
